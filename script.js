b9c3e7fa89b108fece14f4427861e53013c08362
WhipperEarner <143460501+WhipperEarner@users.noreply.github.com>
2023-11-11 04:16:06 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+// Paragraph
+
 const accordion = document.querySelector(".accordion");
 
 accordion.addEventListener("click", (e) => {
@@ -28,3 +31,31 @@ function toggleAccordion(panelToActivate) {
   }
 }
 
+// Scroller
+
+const scrollers = document.querySelectorAll(".scroller");
+
+// If a user hasn't opted in for reduded motion, then we add the animation
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    // add data-animated="true" to every `.scroller` on the page
+    scroller.setAttribute("data-animated", true);
+
+    // Make an array from the elements within `.scroller-inner`
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    // For each item in the array, clone it
+    // add aria-hidden to it
+    // add it into the `.scroller-inner`
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
